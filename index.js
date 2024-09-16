dca61341626f72bfd60075c9ab8ff1dc68fb7705
NazarKutoviy <nazarkutoviy@gmail.com>
2024-09-16 18:53:41 +0300

    фвв

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1
+// let ageNum = prompt("enter your age:");
+// let convertAgeNum = Number(ageNum) + 5;
+// console.log(convertAgeNum)
+// 2
+// let num = parseInt(prompt("Enter number"))
+// if(num % 2 === 0 && num > 10){
+//   console.log("the number is not a prime number and its bigger the 10")
+// }
+// else{
+//   console.log("the number")
+// }
+// 3
+// let num = parseInt(prompt("enter number"))
+// if (num < 50){
+//   console.log ("the number is smaller then 50 or 50 ")
+// }else{
+//     console.log ("the number is bigger then 50 or it ")
+// }
+// 4
+// let gradeNumber = parseInt(prompt("enter your grade number:"));
+// if(gradeNumber === 1){
+//   console.log("very bad grade")
+// }else if (gradeNumber === 2){
+//   console.log("bad grade")
+// }else if (gradeNumber === 3){
+//   console.log("norm grade")
+// }else if (gradeNumber === 4){
+//   console.log("good grade")
+// }else if (gradeNumber === 5){
+//   console.log("wonderful grade")
+// }
+// 5
+// let userName = prompt("enter your name:");
+// let greeting = (userName ==="admin")? "welcome, Admin" :`hi ${userName}`;
+// alert(greeting)
+
